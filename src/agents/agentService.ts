e46714ab0e5e9eed8431c84b6d8b74b4a27eca50
MaxDedecke <jm.dedecke@gmail.com>
2026-01-06 16:59:14 +0100

    Throwaway old agent functionality

--- src/agents/agentService.ts
+++
@@ -1,9 +0,0 @@
-// src/agents/agentService.ts
-
-import { runSystemDetection } from "./systemDetection/runSystemDetection";
-import { runAuthFlow } from "./authFlow/runAuthFlow";
-import { runCapabilityDiscovery } from "./capabilityDiscovery/runCapabilityDiscovery";
-
-export const runSystemDetectionAgent = runSystemDetection;
-export const runAuthFlowAgent = runAuthFlow;
-export const runCapabilityDiscoveryAgent = runCapabilityDiscovery;
--- src/agents/systemDetection/parser.ts
+++
@@ -1,8 +0,0 @@
-// src/agents/systemDetection/parser.ts
-
-import { extractJson } from "../openai/message";
-
-export const parseSystemDetectionResponse = (text: string) => {
-  const jsonStr = extractJson(text);
-  return JSON.parse(jsonStr);
-};
